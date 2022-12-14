89b642fe0b1a90f322fdb0d7fc3ed1dd71c6cc79
Blue Marvel <bluemarvel091@gmail.com>
2022-12-14 06:23:26 -0800

    Edited

--- lib/main.dart
+++ lib/main.dart
@@ -20,81 +20,89 @@ class _NotifyMeState extends State<NotifyMe> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 95, 17, 109),
       body: SingleChildScrollView(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.close_outlined,
-                  size: 60,
-                  color: Colors.white30,
-                )),
-            Container(
-              height: 200,
-              width: 350,
-              decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  image: DecorationImage(
-                    image: AssetImage(
-                        "assets/3d-casual-life-credit-card-and-chart-column.png"),
-                    fit: BoxFit.contain,
-                  )),
-            ),
+          child: Column(
 
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Don't miss a single thing!",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
+
+            // mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(right: 10000),
+                child: IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.close_outlined,
+                      size: 30,
+                      color: Colors.white30,
+                    ),),
               ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Set up push notification so you know when you've been messaged or mentioned",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //Nofitication button
-            InkWell(
-              onTap: null,
-              child: Container(
-                width: 180,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 350,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/notifyMe.png"),
+                            fit: BoxFit.contain,
+                          )),
+                    ),
+                  ],
                 ),
-                child: const Center(
-                  child: Text(
-                    "Turn on notification",
-                    //textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 95, 17, 109),
-                      fontWeight: FontWeight.bold,
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Don't miss a single thing!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Set up push notification so you know when you've been messaged or mentioned",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              //Nofitication button
+              InkWell(
+                onTap: null,
+                child: Container(
+                  width: 180,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Turn on notification",
+                      //textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 95, 17, 109),
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      )),
+            ],
+          )),
     );
   }
 }
